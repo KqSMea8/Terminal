f74b135c9a45db324cd8b1296192c3044db00f65
lockyse7en <lockyse7en@outlook.com>
2018-11-30 03:03:25 +0800

    Initial commit

---
+++ lxrun_bk.sh
@@ -0,0 +1,13 @@
+ #!/bin/bash
+ umask 22
+ mkdir /mnt/f/rootfs
+ cd /mnt/f/rootfs
+mkdir cache data dev home media mnt opt proc root run snap srv sys tmp
+chmod 1777 tmp
+cp -a /b* /etc /l* /sbin /usr /var .
+tar cvpf ../rootfs.tar *
+xz -9 ../rootfs.tar
+cd /mnt/f
+tar cvpf root.tar /root
+tar cvpf home.tar /home
+exit
